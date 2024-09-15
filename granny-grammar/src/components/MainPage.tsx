6c48a8fd4a366b1c37fc26995a24db217b3c9523
cait.chan <cait.chan@gmail.com>
2024-09-15 03:43:02 -0400

    Added popup ux/ui, contributor panel

--- granny-grammar/src/components/MainPage.tsx
+++ granny-grammar/src/components/MainPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { LanguageButton } from "./ui/LanguageButton";
 import { SetLanguagePopup } from "./SetLanguagePopup";
+import { VocabList } from "@/Tracer/VocabList";
+import { ContributorPanel } from "@/Tracer/ContributorPanel";
 
 
 interface MainPageProps {
@@ -15,9 +17,6 @@ export default function MainPage({ selectedLanguage }: MainPageProps) {
     setShowPopup(!showPopup);
   };
 
-
-
-
   return (
     <>
       <div
@@ -26,6 +25,12 @@ export default function MainPage({ selectedLanguage }: MainPageProps) {
       >
         <div className="absolute top-28 left-4 text-black text-xl">
           Currently CoTracing: {selectedLanguage}
+        </div>
+        <div className="absolute top-40 right-4 h-600 w-700">
+            <VocabList className="vocabList absolute border w-300"> </VocabList>
+        </div>
+        <div className="absolute bottom-4 right-4 h-600 w-700">
+            <ContributorPanel className="contributorPanel absolute bottom-4"> </ContributorPanel>
         </div>
         <div className="absolute bottom-4 left-4">
             <button onClick={togglePopup}>
