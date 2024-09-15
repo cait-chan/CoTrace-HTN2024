eb0ccd5ce6f87fa2f393d6eb6d3f033762f3ba1a
Anna Kovtunenko <anya.kovtunenko@gmail.com>
2024-09-15 05:18:46 -0400

    updated submissions.js

--- granny-grammar/convex/submissions.js
+++ granny-grammar/convex/submissions.js
@@ -2,19 +2,17 @@ import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const createTask = mutation({
-  args: {
-    language: v.string(),
-    pronunciation: v.string(),
-    translation: v.string(),
-  },
-  handler: async (ctx, args) => {
-    // Insert the user-provided values into the database
-    const submissionId = await ctx.db.insert("submissions", {
-      language: args.language,
-      pronunciation: args.pronunciation,
-      translation: args.translation,
-    });
-
-    return submissionId; // Optionally return the inserted ID or other info
-  },
+    args: {
+        language: v.string(),
+        pronunciation: v.string(),
+        translation: v.string(),
+    },
+    handler: async (ctx, args) => {
+        // Insert the user-provided values into the database
+        const submissionId = await ctx.db.insert("submissions", {
+            language: args.language,
+            pronunciation: args.pronunciation,
+            translation: args.translation,
+        });
+    },
 });
